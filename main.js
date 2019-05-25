fd4d3fa1665f5bf9bc9c21f1408c45ce6330c94b
arwildo <arwildo@gmail.com>
2019-05-25 22:23:12 +0800

    add nasa apis

--- main.js
+++ main.js
@@ -45,3 +45,30 @@ document.addEventListener('DOMContentLoaded', function (event) {
     }
     StartTextAnimation(0);
 });
+
+// nasa APIs
+var url = "https://api.nasa.gov/planetary/apod?api_key=DrZ0rvpXxkznekZtUtea1GbO2e6chwGx6aC0h6wN";
+
+$.ajax({
+    url: url,
+    success: function (result) {
+        if ("copyright" in result) {
+            $("#copyright").text("Image Credits: " + result.copyright);
+        }
+        else {
+            $("#copyright").text("Image Credits: " + "Public Domain");
+        }
+
+        if (result.media_type == "video") {
+            $("#apod_img_id").css("display", "none");
+            $("#apod_vid_id").attr("src", result.url);
+        }
+        else {
+            $("#apod_vid_id").css("display", "none");
+            $("#apod_img_id").attr("src", result.url);
+        }
+        $("#returnObject").text(JSON.stringify(result, null, 4));
+        $("#apod_explaination").text(result.explanation);
+        $("#apod_title").text(result.title);
+    }
+});
