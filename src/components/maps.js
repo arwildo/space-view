0099dca63b187d53ff77f776ed22c517502587f7
Arwildo <arwildo@gmail.com>
2020-11-17 16:22:04 +0900

    fix close btn size and position

--- src/components/maps.js
+++ src/components/maps.js
@@ -36,10 +36,10 @@ const Maps = (params) => {
 			>
 				{params.mapsOpen && (
 					<div className="absolute shadow-md top-0 h-screen w-screen max-w-screen-xl xl:max-h-full">
-            <div className="absolute top-0 right-0 mr-16 mt-2">
+            <div className="absolute top-0 right-0 mr-16 mt-4">
               <button
                 type="button"
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none bg-white focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                className="inline-flex items-center justify-center p-2 rounded-sm text-gray-500 hover:text-gray-500 hover:bg-gray-100 focus:outline-none bg-white focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                 aria-label="Close map"
                 onClick={() => stopMap(params)}
               >
@@ -52,7 +52,7 @@ const Maps = (params) => {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    strokeWidth={2}
+                    strokeWidth={3}
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
