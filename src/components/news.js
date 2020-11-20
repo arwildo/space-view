116b2a38460341a9f9f085f5c64b513e52a2b687
Arwildo <arwildo@gmail.com>
2020-11-20 15:48:55 +0900

    center article img

--- src/components/news.js
+++ src/components/news.js
@@ -115,7 +115,7 @@ const News = () => {
                     </p>
                     <div className="text-black font-bold text-xl">{article.title}</div>
                   </div>
-                  <img className="w-auto md:w-1/2 px-2" src={article.url}></img>
+                  <img className="w-auto md:w-1/2 px-2 mx-auto" src={article.url}></img>
                   <p className="text-grey-darker text-base mt-4 my-4">{article.explanation}</p>
                   <div className="flex items-center">
                     <img className="w-10 h-10 rounded-full mr-4" src={article.url}></img>
